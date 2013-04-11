a7f8c5cbe7bd2e361729f3ac11fb7586e295a789
ckmarkoh <mark23456@hotmail.com>
2013-04-12 03:51:53 +0800

    remove .o .p .swp

--- src/drawblock.d
+++
@@ -1,2 +0,0 @@
-drawblock.d drawblock.o: drawblock.cpp pdpa1.h bstree.h bstreeMgr.h node.h rnGen.h \
- yheight.h myUsage.h
--- src/main.d
+++
@@ -1,2 +0,0 @@
-main.d main.o: main.cpp pdpa1.h bstree.h bstreeMgr.h node.h rnGen.h yheight.h \
- myUsage.h
