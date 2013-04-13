dab386eba2191307c09e178e79ad9de01a759e61
Mark Chang <mark23456@hotmail.com>
2013-04-13 19:07:08 +0800

    remove *.o *.d

--- src/buildtree.d
+++
@@ -1,2 +0,0 @@
-buildtree.d buildtree.o: buildtree.cpp pdpa1.h bstree.h bstreeMgr.h node.h rnGen.h \
-  yheight.h myUsage.h
--- src/parser.d
+++
@@ -1,2 +0,0 @@
-parser.d parser.o: parser.cpp pdpa1.h bstree.h bstreeMgr.h node.h rnGen.h \
-  yheight.h myUsage.h
